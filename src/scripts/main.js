9e2ee8b8e4f9f54862d631b5376f51cd2f21f992
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-09-09 16:54:26 +0300

    add breadcrumbs, movement, stats, advantages sect

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
+// fixed slider
+
+const headerE1 = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > headerE1.offsetHeight) {
+    headerE1.classList.add("scrolled");
+  } else {
+    headerE1.classList.remove("scrolled");
+  }
+});
+
 const swiper = new Swiper(".feedback .swiper", {
   slidesPerView: 2,
 
