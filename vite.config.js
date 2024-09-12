7af173dc06139a737ba3d69b02ede80deaae5acc
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-09-12 15:43:51 +0300

    add png and build

--- vite.config.js
+++ vite.config.js
@@ -1,68 +1,78 @@
-import vituum from 'vituum';
-import posthtml from '@vituum/vite-plugin-posthtml';
-import postcss from '@vituum/vite-plugin-postcss';
-import { copyFileSync } from 'fs';
+import vituum from "vituum";
+import posthtml from "@vituum/vite-plugin-posthtml";
+import postcss from "@vituum/vite-plugin-postcss";
+import { copyFileSync } from "fs";
 
 export default {
   plugins: [
     vituum(),
     postcss(),
     posthtml({
-      root: './src',
+      root: "./src",
     }),
 
     {
-      name: 'custom-hmr',
-      enforce: 'post',
+      name: "custom-hmr",
+      enforce: "post",
       handleHotUpdate({ file, server }) {
-        if (file.endsWith('.html')) {
+        if (file.endsWith(".html")) {
           server.ws.send({
-            type: 'full-reload',
-            path: '*',
+            type: "full-reload",
+            path: "*",
           });
         }
       },
-    }, 
+    },
     {
-      name: 'copy-static-files',
+      name: "copy-static-files",
       writeBundle() {
-        copyFileSync('src/android-chrome-192x192.png', 'dist/android-chrome-192x192.png');
-        copyFileSync('src/android-chrome-512x512.png', 'dist/android-chrome-512x512.png');
+        copyFileSync(
+          "src/android-chrome-192x192.png",
+          "dist/android-chrome-192x192.png"
+        );
+        copyFileSync(
+          "src/android-chrome-512x512.png",
+          "dist/android-chrome-512x512.png"
+        );
       },
     },
   ],
 
   build: {
-    root: './src',
+    root: "./src",
     rollupOptions: {
       output: {
         assetFileNames: (asset) => {
-          const filePath = asset.name.split('/');
+          const filePath = asset.name.split("/");
           const fileName = filePath.pop();
-          const nestedPath = filePath.join('/');
+          const nestedPath = filePath.join("/");
           const outputPath = `${
-            nestedPath ? nestedPath + '/' : ''
+            nestedPath ? nestedPath + "/" : ""
           }[name][extname]`;
 
-          if (asset.name.includes('favicon') || asset.name.includes('apple-touch-icon') || asset.name.includes('android-chrome') ) {
+          if (
+            asset.name.includes("favicon") ||
+            asset.name.includes("apple-touch-icon") ||
+            asset.name.includes("android-chrome")
+          ) {
             return `${outputPath}`;
           }
 
           console.log(`${asset} - ${asset.name} - ${asset.type}`);
-            console.dir(`${asset}`);
+          console.dir(`${asset}`);
 
-          if (asset.type === 'asset') {
-            switch (asset.name.split('.').pop()) {
-              case 'css':
+          if (asset.type === "asset") {
+            switch (asset.name.split(".").pop()) {
+              case "css":
                 return `css/${outputPath}`;
-              case 'png':
-              case 'jpg':
-              case 'webp':
-              case 'svg':
+              case "png":
+              case "jpg":
+              case "webp":
+              case "svg":
                 return `images/${outputPath}`;
-              case 'woff2':
+              case "woff2":
                 return `fonts/${outputPath}`;
-              case 'webmanifest':
+              case "webmanifest":
                 return `${outputPath}`;
               default:
                 return `other/${outputPath}`;
@@ -70,8 +80,7 @@ export default {
           }
         },
         preserveModuleDirectories: true,
-      }
+      },
     },
   },
 };
-
